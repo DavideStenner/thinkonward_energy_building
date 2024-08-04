162b972fa0b8a84ec1761d584e43a654ae45fd60
Davide Stenner <davide.stenner@cefriel.com>
2024-08-04 14:41:15 +0200

    target col in config

--- src/preprocess/initialize.py
+++ src/preprocess/initialize.py
@@ -52,7 +52,7 @@ class PreprocessInit(BaseInit):
         }
         
         self.tou_unique: list[int] = list(set(self.slice_hour_mapping.values()))
-
+    
     def _initialize_col_list(self) -> None:
         self.build_id: str = self.config_dict['BUILDING_ID']
         with open(
@@ -63,24 +63,10 @@ class PreprocessInit(BaseInit):
         ) as file_dtype:
             self.commercial_index = json.load(file_dtype)['train_label']['building_stock_type']['commercial']
         
-        self.target_col_binary: str = 'building_stock_type'
-        self.target_col_com_list: list[str] = [
-            'in.comstock_building_type_group_com', 'in.heating_fuel_com',
-            'in.hvac_category_com', 'in.number_of_stories_com',
-            'in.ownership_type_com', 'in.vintage_com',
-            'in.wall_construction_type_com', 'in.tstat_clg_sp_f..f_com',
-            'in.tstat_htg_sp_f..f_com', 'in.weekday_opening_time..hr_com',
-            'in.weekday_operating_hours..hr_com',
-        ]
-        self.target_col_res_list: list[str] = [
-            'in.bedrooms_res', 'in.cooling_setpoint_res',
-            'in.heating_setpoint_res', 'in.geometry_building_type_recs_res',
-            'in.geometry_floor_area_res', 'in.geometry_foundation_type_res',
-            'in.geometry_wall_type_res', 'in.heating_fuel_res',
-            'in.income_res', 'in.roof_material_res',
-            'in.tenure_res', 'in.vacancy_status_res',
-            'in.vintage_res',
-        ]
+        self.target_col_binary: str = self.config_dict['TARGET_DICT']['BINARY']
+        self.target_col_com_list: list[str] = self.config_dict['TARGET_DICT']['COMMERCIAL']
+        self.target_col_res_list: list[str] = self.config_dict['TARGET_DICT']['RESIDENTIAL']
+        
         self.all_target_list: list[str] = (
             [self.target_col_binary] +
             self.target_col_com_list +
