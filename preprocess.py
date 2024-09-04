5b5166e0aa38de7a5f5a820e810fd2a964548426
Davide Stenner <davide.stenner@cefriel.com>
2024-09-04 14:14:47 +0200

    additional data preprocess

--- preprocess.py
+++ preprocess.py
@@ -1,8 +1,16 @@
 if __name__=='__main__':
+    import argparse
+    
     from src.utils.import_utils import import_config
     from src.preprocess.pipeline import PreprocessPipeline
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--additional', action='store_true')
 
+    args = parser.parse_args()
+    
     config_dict = import_config()
+    config_dict['additional'] = args.additional
     
     pnl_preprocessor = PreprocessPipeline(
         config_dict=config_dict, 
