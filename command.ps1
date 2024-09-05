4772745c7ae70ec0d73467ffc2bb7c5dc3022295
Davide Stenner <davide.stenner@cefriel.com>
2024-09-05 14:07:26 +0200

    log how many building

--- command.ps1
+++ command.ps1
@@ -7,8 +7,10 @@ $destinationFolder = "data_dump/"
 $stateList = @("AK", "AL", "AR", "AZ", "CA", "CO", "CT", "DC", "DE", "FL", "GA", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MI", "MN", "MO", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VA", "VT", "WA", "WI", "WV", "WY")
 
 # Set the number of files you want to copy
-$N = Read-Host "Enter Number of Home to scrape"
+$N = [int](Read-Host "Enter Number of Home to scrape")
+$EstimatedNBuilding = $N*2*51
 
+Write-Host "Estimated number of new buildings: $EstimatedNBuilding"
 #reset log
 "" | Out-File -FilePath "log/dumping.txt"
 
