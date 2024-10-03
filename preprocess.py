c7ac2a3e028753ceb98d0a2f936f9d335a56eece
Davide Stenner <davide.stenner@cefriel.com>
2024-10-03 11:42:30 +0200

    only inference

--- preprocess.py
+++ preprocess.py
@@ -6,7 +6,8 @@ if __name__=='__main__':
     
     parser = argparse.ArgumentParser()
     parser.add_argument('--add', action='store_true')
-
+    parser.add_argument('--inference', action='store_true')
+    
     args = parser.parse_args()
     
     config_dict = import_config()
@@ -16,7 +17,8 @@ if __name__=='__main__':
         config_dict=config_dict, 
     )
     #train datasets
-    pnl_preprocessor()
+    if not args.inference:
+        pnl_preprocessor()
     
     #also test set
     pnl_preprocessor.begin_inference()
